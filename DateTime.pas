94d77000cf9e9fb82f9aee35d0d0520b1a425102
Artem Burdukovskii <nubasick456@mail.ru>
2024-10-04 23:06:48 +0300

    Add is leap year program

---
+++ DateTime.pas
@@ -0,0 +1,12 @@
+﻿begin
+  var year := ReadInteger('Введите год:');
+  var answer := False;
+  if year mod 4 = 0 then
+  begin
+    if year mod 100 <> 0 then
+      answer := True;
+    if year mod 400 = 0 then
+      answer := True
+  end;
+  Print($'Год весокосный? {answer}');
+end.
