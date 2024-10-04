fd496a336d903e23e72a1f54199fa10d2970491e
Artem Burdukovskii <nubasick456@mail.ru>
2024-10-04 23:27:05 +0300

    Add what day is closer to New Year

--- DateTime.pas
+++ DateTime.pas
@@ -9,4 +9,17 @@
       answer := True
   end;
   Print($'Год весокосный? {answer}');
+  
+  var (N1, N2, M1, M2) := ReadInteger4('Введите две даты: день и месяц:');
+  if M2 > N2 then
+    Print('Вторая дата ближе к Новому Году')
+  else if M2 = N2 then
+  begin
+    if N1 > M1 then
+      Print('Первая дата ближе к Новому Году')
+    else
+      Print('Вторая дата ближе к Новому Году');
+  end
+  else
+    Print('Первая дата ближе к Новому Году');
 end.
